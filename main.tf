26df21261c20ceb46cb632f21bf259ce248094e6
Daniel Mezentsev <dan@soleks.com>
2024-06-04 15:55:41 -0700

    removed hardcoded IAM statements

--- main.tf
+++ main.tf
@@ -13,58 +13,6 @@ resource "aws_iam_policy" "ec2_iam_policy" {
   name        = "${var.name_prefix}-iam-policy"
 
   policy = data.aws_iam_policy_document.ec2_iam_policy_document.json
-
-}
-
-resource "aws_iam_policy" "ec2_iam_policy_old" {
-  name        = "${var.name_prefix}-iam-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:DescribeInstances",
-          "ec2:DescribeTags",
-          "ec2:DescribeVolumes",
-          "ec2:AttachVolume",
-          "ec2:DetachVolume"
-        ]
-        Effect   = "Allow"
-        Resource = [
-          "*"
-        ]
-      }, 
-      {
-        Action = [
-          "s3:ListBucket",
-          "s3:GetObject"
-        ]
-        Effect   = "Allow"
-        Resource = [
-          "arn:aws:s3:::talkscriber-nvidia",
-          "arn:aws:s3:::talkscriber-nvidia/*",
-        ]
-      },
-      {
-        Action = [
-          "s3:*"
-        ]
-        Effect   = "Allow"
-        Resource = [
-          "arn:aws:s3:::ts-postgres",
-          "arn:aws:s3:::ts-postgres/*",
-        ]
-      },
-      {
-        Action = [
-          "ecr:*"
-        ]
-        Effect   = "Allow"
-        Resource = [ "*" ]
-      }
-    ]
-  })
 }
 
 resource "aws_iam_role" "ec2_iam_role" {
@@ -87,11 +35,11 @@ resource "aws_iam_role" "ec2_iam_role" {
 
 resource "aws_iam_policy_attachment" "ec2_iam_attachment" {
   name       = "${var.name_prefix}-iam-attachment"
-  roles      = [aws_iam_role.iam_role.name]
-  policy_arn = aws_iam_policy.iam_policy.arn
+  roles      = [aws_iam_role.ec2_iam_role.name]
+  policy_arn = aws_iam_policy.ec2_iam_policy.arn
 }
 
 resource "aws_iam_instance_profile" "ec2_iam_instance_profile" {
   name = "${var.name_prefix}-iam-profile"
-  role = aws_iam_role.iam_role.name
+  role = aws_iam_role.ec2_iam_role.name
 }
